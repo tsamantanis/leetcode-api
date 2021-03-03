f84673179a6a8f1c9d5d33bc49b984f0295ba665
tsamantanis <philip.tsamantanis@icloud.com>
2021-03-03 04:11:47 -0800

    Save question ids that have been received by a user to avoid sending them a question they have seen

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 const fetch = require('node-fetch');
 const { validationResult } = require('express-validator');
 const { getRandomInt } = require('../helpers/random');
 const auth = require('./auth');
+const User = mongoose.model('User');
 
 router.get('/all', auth.required, async (req, res) => {
     const errors = validationResult(req);
@@ -48,7 +50,18 @@ router.get('/free/random', auth.required, async (req, res) => {
     const body = await response.text()
     let data = JSON.parse(body)
     data = data.stat_status_pairs.filter((question) => !question.paid_only)
-    return res.json(data[getRandomInt(data.length - 1)])
+    const user = await User.findById(req.payload.id)
+    let newQuestion = null;
+    let temp
+    while (!newQuestion) {
+        temp = data[getRandomInt(data.length - 1)]
+        if (!user.questions.includes(temp.stat.question_id)) {
+            user.questions.unshift(temp.stat.question_id)
+            newQuestion = temp
+        }
+    }
+    await user.save()
+    return res.json(newQuestion)
 })
 
 router.get('/paid/random', auth.required, async (req, res) => {
